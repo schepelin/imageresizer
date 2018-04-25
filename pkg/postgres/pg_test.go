28f7676ffd877073373825211c1f9a5ea95917ec
Maxim Schepelin <m.schepelin@gmail.com>
2018-04-25 10:50:24 +0300

    GETT-24312 Create all tests with real postgres DB

--- pkg/postgres/pg_test.go
+++ pkg/postgres/pg_test.go
@@ -14,7 +14,6 @@ import (
 	"testing"
 	"time"
 	"v/github.com/rubenv/sql-migrate@v0.0.0-20180217203553-081fe17d19ff"
-	"v/gopkg.in/DATA-DOG/go-sqlmock.v1@v1.3.0-gopkgin-v1.3.0"
 )
 
 func getRawImageSample() []byte {
@@ -111,67 +110,48 @@ func TestNewPostgresStorage(t *testing.T) {
 	// TODO: How to check ps
 }
 
-func TestPostgresStorage_Create(t *testing.T) {
-	db, mock, err := sqlmock.New()
-	if err != nil {
-		t.Error("error creating mock database")
-
-	}
-	defer db.Close()
-	ps := PostgresStorage{db}
-	ctx := context.TODO()
-	imgModel := storage.ImageModel{
-		Id:        "100500",
-		Raw:       getRawImageSample(),
-		CreatedAt: time.Date(1970, time.January, 1, 0, 0, 0, 0, time.UTC),
-	}
-	mock.ExpectExec("INSERT INTO images").WithArgs(imgModel.Id, string(imgModel.Raw), imgModel.CreatedAt)
-	ps.Create(ctx, &imgModel)
-
-	assert.NoError(t, mock.ExpectationsWereMet())
-}
-
 func TestPostgresStorage_Get(t *testing.T) {
-	db, mock, _ := sqlmock.New()
-	defer db.Close()
-	ps := PostgresStorage{db}
+	var deps dependencies
+	var err error
+	defer preparator(t, &deps)()
+	ps := PostgresStorage{deps.db}
+
 	ctx := context.TODO()
 	imgId := "42"
 	expectedRaw := []byte{10, 42, 15}
-	expectedCreatedAt := time.Date(1970, time.January, 1, 0, 0, 0, 0, time.UTC)
-	rows := sqlmock.NewRows([]string{"raw", "created_at"}).AddRow(
-		string(expectedRaw),
-		expectedCreatedAt,
-	)
-	mock.ExpectQuery(
-		"SELECT raw, created_at FROM images WHERE id=?",
-	).WithArgs(imgId).WillReturnRows(rows)
+	expectedCreatedAt := time.Date(1970, time.January, 1, 0, 0, 0, 0, time.FixedZone("", 0))
+	ps.DB.Exec("INSERT INTO images(id, raw, created_at) VALUES ($1, $2, $3)", imgId, string(expectedRaw), expectedCreatedAt)
 
 	imgModel, err := ps.Get(ctx, imgId)
 
 	assert.NoError(t, err)
-	assert.NoError(t, mock.ExpectationsWereMet())
-	assert.Equal(t, imgModel.Id, imgId)
-	assert.Equal(t, imgModel.Raw, expectedRaw)
-	assert.Equal(t, imgModel.CreatedAt, expectedCreatedAt)
+	assert.Equal(t, imgId, imgModel.Id)
+	assert.Equal(t, expectedRaw, imgModel.Raw)
+	assert.Equal(t, expectedCreatedAt, imgModel.CreatedAt)
+
 }
 
 func TestPostgresStorage_Delete(t *testing.T) {
-	db, mock, _ := sqlmock.New()
-	defer db.Close()
-	ps := PostgresStorage{db}
+	var deps dependencies
+	var err error
+	defer preparator(t, &deps)()
+	ps := PostgresStorage{deps.db}
+
 	ctx := context.TODO()
-	imgId := "42"
+	imgId := "42010"
+	raw := []byte{42, 10, 15}
+	createdAt := time.Date(1970, time.January, 1, 0, 0, 1, 0, time.FixedZone("", 0))
 
-	mock.ExpectExec(
-		"DELETE FROM images WHERE id=?",
-	).WithArgs(imgId).WillReturnResult(sqlmock.NewResult(100500, 1))
+	ps.DB.Exec("INSERT INTO images(id, raw, created_at) VALUES ($1, $2, $3)", imgId, raw, createdAt)
 
-	ps.Delete(ctx, imgId)
-	assert.NoError(t, mock.ExpectationsWereMet())
+	err = ps.Delete(ctx, imgId)
+
+	var selectedId string
+	err = ps.DB.QueryRow("SELECT id from images where id=$1", imgId).Scan(&selectedId)
+	assert.Equal(t, sql.ErrNoRows, err)
 }
 
-func TestPostgresStorage_CreateWithDbHelper(t *testing.T) {
+func TestPostgresStorage_Create(t *testing.T) {
 	var deps dependencies
 	defer preparator(t, &deps)()
 	ps := PostgresStorage{deps.db}
@@ -192,51 +172,7 @@ func TestPostgresStorage_CreateWithDbHelper(t *testing.T) {
 		&actualCreatedAt,
 	)
 	assert.NoError(t, err)
-	assert.Equal(t, []byte(actualRaw), imgModel.Raw)
+	assert.Equal(t, imgModel.Raw, []byte(actualRaw))
 
-	assert.Equal(t, actualCreatedAt, imgModel.CreatedAt)
-}
-
-func TestPostgresStorage_CreateWithRealDB(t *testing.T) {
-	const dbConnect string = "postgres://localhost/imageres_dev?sslmode=disable"
-	migrationsHistory := &migrate.AssetMigrationSource{
-		Asset:    migrations.Asset,
-		AssetDir: migrations.AssetDir,
-		Dir:      "migrations",
-	}
-
-	testDbName := "imgres_dev_test_" + fmt.Sprintf("%v", time.Now().UnixNano())
-
-	ps, err := NewPostgresStorage(dbConnect)
-	if err != nil {
-		t.Error("Could not connect to the databse")
-	}
-	originalDb := ps.DB
-	defer originalDb.Close()
-	originalDb.Exec("CREATE DATABASE " + testDbName + " ENCODING 'UTF8'")
-	db, err := sql.Open("postgres", "postgres://localhost/"+testDbName+"?sslmode=disable")
-
-	if err != nil {
-		t.Error(err)
-	}
-
-	_, err = migrate.Exec(db, "postgres", migrationsHistory, migrate.Up)
-	if err != nil {
-		t.Error(err)
-	}
-
-	ps.DB = db
-	ctx := context.TODO()
-	imgModel := storage.ImageModel{
-		Id:        "100500",
-		Raw:       getRawImageSample(),
-		CreatedAt: time.Date(1970, time.January, 1, 0, 0, 0, 0, time.UTC),
-	}
-	ps.Create(ctx, &imgModel)
-	db.Close()
-	_, err = originalDb.Exec("DROP DATABASE " + testDbName)
-	if err != nil {
-		t.Error(err)
-	}
-
+	assert.Equal(t, imgModel.CreatedAt, actualCreatedAt)
 }
